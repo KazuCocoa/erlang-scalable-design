2aecb0530c10d5c131b89b9d6dea842b3ceb7116
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2016-02-21 01:05:34 +0900

    add sample of timeout

---
+++ timeout.erl
@@ -0,0 +1,11 @@
+-module (timeout).
+-behaviour (gen_server).
+
+-export ([init/1, handle_call/3]).
+
+init(_Args) ->
+  {ok, null}.
+
+handle_call({sleep, Ms}, _From, LoopData) ->
+  timer:sleep(Ms),
+  {reply, ok, LoopData}.
