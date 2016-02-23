e1ef9063acf3074965ae950116960a92a79f79d4
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2016-02-24 00:55:21 +0900

    add script

---
+++ erlang/bsc_sup.erl
@@ -0,0 +1,19 @@
+-module (bsc_sup).
+-behaviour (supervisor).
+
+-export ([start_link/0, init/1]).
+-export ([stop/0]).
+
+start_link() ->
+  {ok, Pid} = supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+stop() ->
+  exit(whereis(?MODULE), shutdown).
+
+init(_) ->
+  ChildSpecList = [child(overload), child(frequency), child(simple_phone_sup)],
+  {ok, {{rest_for_one, 2, 3600}, ChildSpecList}}.
+
+child(Module) ->
+  {Module, {Module, start_link, []},
+  permanent, 2000, worker, [Module]}.
